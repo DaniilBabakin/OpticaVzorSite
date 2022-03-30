0656dbad20a20f0f00e29dc930fa8a11f69d5c8c
Daniil Babakin <rrdreaming@mail.ru>
2022-03-30 14:42:53 +0300

    second commit

--- src/components/products/productsPage/FilterMenu/FilterMenu.js
+++ src/components/products/productsPage/FilterMenu/FilterMenu.js
@@ -1,22 +1,78 @@
 import { useState } from 'react'
 import './FilterMenu.scss'
-export default function FilterMenu({active,setActive}){
-  const [priceOpen,setPriceOpen] = useState(false)
+export default function FilterMenu({active,setActive,setFilter,filterValue,productsInfo,setProductsInfo}){
+  const [priceFilterValue,setPriceFilterValue] = useState("") // По убыванию / по возрастанию
+
+  const handleClick = (e) => {
+    !filterValue.includes(e.target.attributes.value.value) ? setFilter(oldArray => [...oldArray, e.target.attributes.value.value]) : setFilter( filterValue.filter((item) => !item.includes(e.target.attributes.value.value)) )
+  }
+  const handlePriceChange = (value) => {
+    if (value === "По возрастанию"){
+      setPriceFilterValue(value)
+      setProductsInfo(productsInfo.sort((a,b)=>a.newPrice - b.newPrice))
+    }
+    if (value === "По убыванию"){
+      setPriceFilterValue(value)
+      setProductsInfo(productsInfo.sort((a,b)=>b.newPrice - a.newPrice))
+    }
+    if (value === "По умолчанию") {
+      setPriceFilterValue(value)
+      setProductsInfo(productsInfo.sort(() => Math.random() - 0.5))
+    }
+  }
+  console.log(filterValue.length)
+  console.log(priceFilterValue == "")
+  //Состояния списков фильтрации
+  const [priceOpen,setPriceOpen] = useState(false)  
+  const [glassesFormOpen,setGlassesFormOpen] = useState(false)  
+
   return(
     <div className={"menu__container " + (active ? 'active' : '')} onClick={()=>setActive(false)}>
+
+
+      {/* Header */}
       <div className="menu__div" onClick={e=>e.stopPropagation()}>
-        <h2>Сортировка</h2>
-        
+        <div className="menu__header">
+          <h2>Сортировка</h2>
+          <button id='close' onClick={()=>setActive(false)}>close</button>
+        </div>
+
+
+      {/* Контейнер с включенными фильтрами */}
+       {filterValue.length !== 1 || priceFilterValue !== ""  // Показать контейнер , если один из фильтров включен
+        ? (
+          <div className='currentFilters'> 
+            {filterValue.slice(1).map((item)=> <span className='currentFilters__span' onClick={()=>{setFilter( filterValue.filter((value) => !value.includes(item)))}}>{item}</span>)}
+
+            {priceFilterValue !== "" && <span className='currentFilters__span' onClick={()=>{setPriceFilterValue("");setProductsInfo(productsInfo.sort(() => Math.random() - 0.5))}}>{  priceFilterValue}</span>}
+          </div>
+          ) 
+        : null}
+
+
+      {/* Выбрать фильтр */}  
         <div className='select__div'>
           <div className={priceOpen ? "select__title__div" : "select__title__div border"} onClick={()=>setPriceOpen(!priceOpen)}>
-            <span title="Sort By" className="select__title">Sort By</span>
+            <span title="Цена" className="select__title">Цена</span>
           </div>
           <div className={priceOpen ? 'options__container open' : 'options__container'}>
               <ul class="sorting__options__wrapper">
-                <li class="sorting__option">Newest</li>
-                <li class="sorting__option">Price (high - low)</li>
-                <li class="sorting__option">Price (low - high)</li>
-                <li class="sorting__option">Top Sellers</li>
+                <li class="sorting__option" onClick={()=>handlePriceChange("По умолчанию")}>по умолчанию</li>
+                <li class="sorting__option" onClick={()=>handlePriceChange("По убыванию")}>по убыванию</li>
+                <li class="sorting__option" onClick={()=>handlePriceChange("По возрастанию")}>по возрастанию</li>
+              </ul>
+          </div>
+        </div>
+        <div className='select__div'>
+          <div className={glassesFormOpen ? "select__title__div" : "select__title__div border"} onClick={()=>setGlassesFormOpen(!glassesFormOpen)}>
+            <span title="Цена" className="select__title">Форма оправы</span>
+          </div>
+          <div className={glassesFormOpen ? 'options__container open' : 'options__container'}>
+              <ul class="sorting__options__wrapper">
+                <li class="sorting__option" value={"Круглые"} onClick={handleClick}>Круглые</li>
+                <li class="sorting__option" value={"Квадратные"} onClick={handleClick}>Квадратные</li>
+                <li class="sorting__option" value={"Прямоугольные"} onClick={handleClick}>Прямоугольные</li>
+                <li class="sorting__option" value={"Овальные"} onClick={handleClick}>Овальные</li>
               </ul>
           </div>
         </div>
--- src/components/products/productsPage/productsPage.js
+++ src/components/products/productsPage/productsPage.js
@@ -1,4 +1,4 @@
-import { useState} from "react"
+import { useEffect, useState} from "react"
 import { productsPageProducts } from "../../../ListOfProducts"
 import '../main-products/main-products.css'
 import Button from "./Button"
@@ -12,28 +12,29 @@ export default function ProductPageProducts(){
   const [currentPage,setCurrentPage] = useState(1) //Пагинация
   const [productsPerPage] = useState(10) //Количество продуктов на страницу(Пагинация)
   const [active,setActive] = useState(false) //Состояние модального окна фильтрации
+  const [productsInfo,setProductsInfo] = useState(productsPageProducts)
 
   //Для работы Пагинации
   const lastProductIndex = currentPage * productsPerPage //Последний индекс продукта на странице
   const firstProductIndex = lastProductIndex - productsPerPage  //Первый 
-  const currentProducts = productsPageProducts.filter((item)=> item.category.join("").includes(filter.sort().join(""))).slice(firstProductIndex,lastProductIndex) //текущие продукты на странице
+  const currentProducts = productsInfo.filter((item)=> item.category.join("").includes(filter.sort().join(""))).slice(firstProductIndex,lastProductIndex) //текущие продукты на странице
   const currentProductsCount = productsPageProducts.filter((item)=> item.category.join("").includes(filter.sort().join(""))) //количество продуктов в зависимости от фильтров
-
+  
+  console.log(currentProducts.length)
+  
+  
   const paginate = pageNumber => setCurrentPage(pageNumber)
-  console.log(currentProducts)
   return(
     <section className="products__div">
-      <FilterMenu active={active} setActive={setActive}/>
-      {/* Меню фильтрации */}
-      <aside className="products__aside" style={{width:"200px",background:"#c4c4c4",height:"100px"}}>
-      <button onClick={()=>setActive(true)}>ФильтрМеню</button>
-      </aside>
+      <FilterMenu active={active} setActive={setActive} setFilter={setFilter} filterValue={filter} productsInfo={productsInfo} setProductsInfo={setProductsInfo}/>
 
       {/* Основной каталог продуктов */}
       <div className="products products__container" style={{display:"flex",marginLeft:"auto",flexDirection:"row",flexWrap:"wrap",alignContent:"start",justifyContent:"space-evenly"}}>
-        {currentProducts
+      <button class="button-23" role="button" onClick={()=>setActive(true)}>Фильтры</button>
+
+      {currentProducts.length !== 0 ? (currentProducts
             .filter((item)=> item.category.join("").includes(filter.sort().join("")))
-            .map(item =>  <div className="product__item" ><ProductItem key={item.id} item={item}/></div>)}
+            .map(item =>  <div className="product__item" ><ProductItem key={item.id} item={item}/></div>)) : (<p>Не найдено,попробуйте изменить фильтры</p>)}
       </div>
 
       {/* Контейнер для пагинации(кнопки) */}
