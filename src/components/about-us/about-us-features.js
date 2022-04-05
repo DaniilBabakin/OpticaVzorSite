65d71536767ecc16cc24be82ac8e10d7db9a3bc7
DaniilBabakin <rrdreaming@mail.ru>
2022-04-05 17:32:22 +0300

    5 commit

---
+++ src/components/about-us/about-us-features.js
@@ -0,0 +1,31 @@
+export default function AboutUsFeatures (){
+  <div style={{width:"100%",backgroundColor:"white",padding:"30px 0"}}>
+  <div style={{maxWidth:"1024px",margin:"0 auto",display:"flex",flexDirection:"row",flexWrap:"wrap",justifyContent:"center"}}>
+
+    <div style={{display:"flex",flexDirection:"column",justifyItems:"center",alignItems:"center",width:"29%",minWidth:"160px",margin:"15px auto"}}>
+        <svg xmlns="http://www.w3.org/2000/svg" style={{width:"50px"}} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+        </svg>
+      <h3 style={{textAlign:"center"}}>Название преимущества</h3>
+      <p style={{maxWidth:"130px",margin:"0",textAlign:"center"}}>Описание преимущества</p>
+    </div>
+
+    <div style={{display:"flex",flexDirection:"column",justifyItems:"center",alignItems:"center",width:"29%",minWidth:"160px",margin:"15px auto"}}>
+        <svg xmlns="http://www.w3.org/2000/svg" style={{width:"50px"}} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+        </svg>
+      <h3 style={{textAlign:"center"}}>Название преимущества</h3>
+      <p style={{maxWidth:"130px",margin:"0",textAlign:"center"}}>Описание преимущества</p>
+    </div>
+
+    <div style={{display:"flex",flexDirection:"column",justifyItems:"center",alignItems:"center",width:"29%",minWidth:"160px",margin:"15px auto"}}>
+        <svg xmlns="http://www.w3.org/2000/svg" style={{width:"50px"}} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+        </svg>
+      <h3 style={{textAlign:"center"}}>Название преимущества</h3>
+      <p style={{maxWidth:"130px",margin:"0",textAlign:"center"}}>Описание преимущества </p>
+    </div>
+    
+  </div>
+</div>
+}
--- src/components/products/productsPage/ProductItem.js
+++ src/components/products/productsPage/ProductItem.js
@@ -4,13 +4,27 @@ export default function ProductItem({item}) {
     <div className="containerCard">
      {/*Карточка продукта */}
       <div className="productCard">
-        <div className="imgBx">
+
+      {/*Название , если товар - линза */}
+      {item.lensText !== undefined && (
+        <div style={{width:"100%",textAlign:"center",background:"#f27f38",padding:"5px 0px",color:"#f5f5f5"}}>
+          <span style={{textAlign:"center",justifyContent:"center",width:"100%",fontWeight:"bold",fontSize:"18px"}}>{item.name}</span>
+        </div>
+      )}        
+      {/*Изображение или текст(если это линза) */}
+      {item.lensText == undefined ?(
+          <div className="imgBx">
             <img src={item.url} alt="img" className="products__image"/>
-        </div>
-        <div className="product__name">
-          {item.name}
+          </div>
+        ) : (<p className="lens__text">{item.lensText}</p>)}
+
+      {/*Название , если товар - очки */}
+        {item.lensText == undefined && (
+          <div className="product__name">
+            {item.name}
+          </div>
+        )}
         
-        </div>
       </div>
       {/*Цены*/}
       <span className="card__subname">{item.subName}</span>
